a4a2d83601580018ae23df4280d9c4578bea6ea5
Caleb Whitmer <caleb.s.whitmer@gmail.com>
2026-04-04 21:47:56 -0400

    fix: logarithms of images width error

--- app/Main.hs
+++ app/Main.hs
@@ -11,12 +11,12 @@ import Droste
 
 main :: IO ()
 main = do
-    imRead <- readPng "canvas1.png"
+    imRead <- readPng "test.png"
     case imRead of
         Left err -> putStrLn err
         Right imSuc -> do
             let img = convertRGB8 imSuc
             let imgOut = Droste.log img (1/16)
-            writePng "test2.png" imgOut
+            writePng "test3.png" imgOut
             -- putStrLn (show (pixelAt img 1 1))
             -- putStrLn (show (imageWidth img))
--- src/Droste.hs
+++ src/Droste.hs
@@ -7,8 +7,7 @@ import Codec.Picture
 import Complex
 
 -- todo:
---   figure out width  discrepancy
---   non-centered Drostes
+--   non-centered Droste
 --   implement exp of image function
 
 log :: Image PixelRGB8 -> Float -> Image PixelRGB8
@@ -16,22 +15,34 @@ log img scale = generateImage imgNew
   (floor width)
   (floor height)
     where 
-      imgNew x y = pixelAt img (floor x') (floor y')
-        where
-          y' = (1-(imaginary p3)-pi) * 
-               inHeight
-          x' = (real p3) * inWidth
-          p3 = (Complex.exp(p2) * sc) + 
-               (Complex 0.5 (0.5 - pi))
-          sc = (Complex (1/(2*e)) 0)
-          e  = Prelude.exp(1)
-          p2 = Complex ((s*xn) - s + 1) 
-               (-2 * yn)
-          s  = Prelude.log(1/scale)
-          yn = (fromIntegral y) / inHeight
-          xn = (fromIntegral x) / inWidth
-      height = inHeight * pi
-      width = inWidth
-      -- width = inWidth * scale * 4
+      imgNew x y = pixelAt img 
+        (floor x')
+        (floor y')
+          where
+            y' = (1-(imaginary p3)-pi)* -- Get y component (imaginary) placing
+                 inHeight               -- it in first quadrant and scaling.
+                                        -- 
+            x' = (real p3) * inWidth    -- Get x component (real & 1st quad).
+                                        -- 
+            p3 = (Complex.exp(p2)*sc) + -- Final point; placed at some radius
+                 (Complex 0.5 (0.5-pi)) -- and angle from middle of input image.
+                                        -- 
+            sc = (Complex (1/(2*e)) 0)  -- Scale p3 by this constant.
+                                        -- 
+            e  = Prelude.exp(1)         -- Euler's number.
+                                        -- 
+            p2 = Complex ((s*xn)-s+1)   -- 
+                 (-2 * yn)              -- Intermediate point.
+                                        -- 
+            s  = Prelude.log(1 / scale) -- Factor to scale x component of p2.
+                                        -- 
+            yn = (fromIntegral y) *     -- 
+                 (pi/height)            -- Normalize y between 0 and pi.
+                                        -- 
+            xn = (fromIntegral x) *     -- 
+                 (1/width)              -- Normalize x between 0 and 1.
+                                        -- 
+      height = inHeight * pi            -- Height is max circumference of input.
+      width = inWidth * (1-scale) * 0.5 -- Width is one minus scale all over 2.
       inHeight = (fromIntegral (imageHeight img) :: Float)
       inWidth = (fromIntegral (imageWidth img) :: Float)
